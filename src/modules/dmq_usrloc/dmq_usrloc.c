f74b9e9af67f1904d88a981bf8e1761c7146b2d3
Stefan Mititelu <stefan.mititelu@net2phone.com>
2025-03-07 19:38:49 +0200

    Patches so far

--- src/modules/dmq_usrloc/dmq_usrloc.c
+++ src/modules/dmq_usrloc/dmq_usrloc.c
@@ -37,12 +37,14 @@ static int child_init(int);
 int dmq_usrloc_enable = 0;
 int _dmq_usrloc_sync = 1;
 int _dmq_usrloc_replicate_socket_info = 0;
+int _dmq_usrloc_replicate_cflags = 1;
 int _dmq_usrloc_batch_size = 0;
 int _dmq_usrloc_batch_msg_contacts = 1;
 int _dmq_usrloc_batch_msg_size = 60000;
 int _dmq_usrloc_batch_usleep = 0;
 str _dmq_usrloc_domain = str_init("location");
 int _dmq_usrloc_delete = 1;
+int _dmq_usrloc_delete_expired = 0;
 
 usrloc_api_t dmq_ul;
 
@@ -53,12 +55,15 @@ static param_export_t params[] = {
 	{"enable", INT_PARAM, &dmq_usrloc_enable},
 	{"sync", INT_PARAM, &_dmq_usrloc_sync},
 	{"replicate_socket_info", INT_PARAM, &_dmq_usrloc_replicate_socket_info},
+	{"replicate_cflags", INT_PARAM, &_dmq_usrloc_replicate_cflags},
 	{"batch_msg_contacts", INT_PARAM, &_dmq_usrloc_batch_msg_contacts},
 	{"batch_msg_size", INT_PARAM, &_dmq_usrloc_batch_msg_size},
 	{"batch_size", INT_PARAM, &_dmq_usrloc_batch_size},
 	{"batch_usleep", INT_PARAM, &_dmq_usrloc_batch_usleep},
 	{"usrloc_domain", PARAM_STR, &_dmq_usrloc_domain},
 	{"usrloc_delete", INT_PARAM, &_dmq_usrloc_delete},
+	{"usrloc_delete", INT_PARAM, &_dmq_usrloc_delete},
+	{"usrloc_delete_expired", INT_PARAM, &_dmq_usrloc_delete_expired},
 	{0, 0, 0}
 };
 
@@ -116,6 +121,7 @@ static int mod_init(void)
 			LM_ERR("Error in dmq_usrloc_initialize()\n");
 		}
 	}
+
 	return 0;
 }
 
