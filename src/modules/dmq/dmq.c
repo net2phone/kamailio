3c0168a3c507d03346f10f43a8f201fb5bf59c47
Stefan Mititelu <stefan.mititelu@net2phone.com>
2025-07-22 13:30:27 +0300

    Local patches so far

--- src/modules/dmq/dmq.c
+++ src/modules/dmq/dmq.c
@@ -66,6 +66,10 @@ str dmq_notification_channel = str_init("notification_peer");
 int dmq_multi_notify = 0;
 static sip_uri_t dmq_notification_uri = {0};
 int dmq_ping_interval = 60;
+int dmq_remove_inactive = 1;
+int dmq_fail_count_enabled = 0;
+int dmq_fail_count_threshold_not_active = 0;
+int dmq_fail_count_threshold_disabled = 1;
 
 /* TM bind */
 struct tm_binds _dmq_tmb = {0};
@@ -123,6 +127,10 @@ static param_export_t params[] = {
 	{"notification_channel", PARAM_STR, &dmq_notification_channel},
 	{"multi_notify", INT_PARAM, &dmq_multi_notify},
 	{"worker_usleep", INT_PARAM, &dmq_worker_usleep},
+	{"remove_inactive", INT_PARAM, &dmq_remove_inactive},
+	{"fail_count_enabled", INT_PARAM, &dmq_fail_count_enabled},
+	{"fail_count_threshold_not_active", INT_PARAM, &dmq_fail_count_threshold_not_active},
+	{"fail_count_threshold_disabled", INT_PARAM, &dmq_fail_count_threshold_disabled},
 	{0, 0, 0}
 };
 
@@ -419,11 +427,11 @@ static void dmq_rpc_list_nodes(rpc_t *rpc, void *c)
 		ip_addr2sbuf(&cur->ip_address, ip, IP6_MAX_STR_SIZE);
 		if(rpc->add(c, "{", &h) < 0)
 			goto error;
-		if(rpc->struct_add(h, "SSssSdd", "host", &cur->uri.host, "port",
+		if(rpc->struct_add(h, "SSssSddd", "host", &cur->uri.host, "port",
 				   &cur->uri.port, "proto", get_proto_name(cur->uri.proto),
 				   "resolved_ip", ip, "status", dmq_get_status_str(cur->status),
 				   "last_notification", cur->last_notification, "local",
-				   cur->local)
+				   cur->local, "fail_count", cur->fail_count)
 				< 0)
 			goto error;
 		cur = cur->next;
